a21a379ab371c30a03141d58d44267eeef90940f
Maximo <maximorossini2016@gmail.com>
2022-04-05 11:10:48 -0300

    added dotenv-react to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const Dotenv = require("dotenv-webpack");
 const LiveServer = require("live-server");
 const path = require("path");
 const dev = process.env.NODE_ENV === "development";
@@ -51,4 +52,5 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
+  plugins: [new Dotenv()],
 };
